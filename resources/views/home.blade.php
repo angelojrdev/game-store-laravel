1fea088d3b4080c2f9e3df6a7bf13b4b5242a9d4
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-08-18 06:40:20 -0300

    Create home page with navbar

---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>{{ config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="bg-slate-900">
+    <nav class="bg-black text-white p-5 flex items-center">
+        <a href="{{ route('home') }}" class="text-xl font-black hover:text-neutral-300 transition">
+            {{ config('app.name') }}
+        </a>
+
+        <ul class="font-semibold ml-6 flex items-center gap-4">
+            <li>
+                <a href="#" class="hover:text-neutral-300 transition">Games</a>
+            </li>
+            <li>
+                <a href="#" class="hover:text-neutral-300 transition">News</a>
+            </li>
+            <li>
+                <a href="#" class="hover:text-neutral-300 transition">About</a>
+            </li>
+        </ul>
+
+        <div class="font-semibold ml-auto flex items-center gap-4">
+            <a href="#" class="px-2 py-1 border border-blue-500 rounded
+                               hover:bg-blue-500 transition">
+                Sign Up
+            </a>
+            <a href="#" class="px-2 py-1 border border-blue-500 rounded
+                               hover:bg-blue-500 transition">
+                Log In
+            </a>
+        </div>
+    </nav>
+</body>
+
+</html>
