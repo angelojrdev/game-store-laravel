1d4c840d527337970e718b39bf7c78778c2af4d9
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-08-25 14:55:00 -0300

    Create and setup AuthenticatedSessionController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
 Route::view('/signup', 'signup')->name('signup');
-Route::view('/login', 'login')->name('login');
+
+Route::controller(AuthenticatedSessionController::class)->group(function () {
+    Route::view('/login', 'login')->name('login');
+
+    Route::post('/login', 'store')->name('login.store');
+    Route::post('/logout', 'destroy')->name('logout');
+});
