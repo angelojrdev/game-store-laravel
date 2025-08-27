26d130632c923463c70d46e1819d687538e62d08
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-08-27 16:37:59 -0300

    Simplify AuthenticatedSessionController name into AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthenticatedSessionController extends Controller
+class AuthController extends Controller
 {
     public function store(Request $request)
     {
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
 Route::view('/signup', 'signup')->name('signup');
 
-Route::controller(AuthenticatedSessionController::class)->group(function () {
+Route::controller(AuthController::class)->group(function () {
     Route::view('/login', 'login')->name('auth.create');
     Route::post('/login', 'store')->name('auth.store');
     Route::post('/logout', 'destroy')->name('auth.destroy');
