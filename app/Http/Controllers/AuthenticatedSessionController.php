7a9e6fc075b11ffaf33b3dc60b81c4907a41497a
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-08-27 15:24:24 -0300

    Add login and logout actions

--- app/Http/Controllers/AuthenticatedSessionController.php
+++ app/Http/Controllers/AuthenticatedSessionController.php
@@ -3,16 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'username' => ['required'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($validated)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended();
+        }
+
+        return back()->withErrors([
+            'username' => "Couldn't authenticate with provided credentials.",
+        ])->onlyInput('username');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
-        //
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
