438710e4f71f475b0b99648a6b5cf6495dde850f
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-09-13 09:44:03 -0300

    Add signup feature

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -2,7 +2,7 @@
     <div class="mx-auto max-w-md bg-black p-6 rounded-lg">
         <h1 class="text-2xl font-bold mb-4 text-center">Create account</h1>
 
-        <form>
+        <form action="{{ route('signup.post') }}" method="POST">
             @csrf
 
             <x-form-field label="Full Name" name="full_name" type="text" />
@@ -13,7 +13,7 @@
 
             <x-form-field label="Password" name="password" type="password" />
 
-            <x-form-field label="Password Confirmation" name="password_confirmation" type="password_confirmation" />
+            <x-form-field label="Password Confirmation" name="password_confirmation" type="password" />
 
             <button class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition">
                 Submit
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NewsPostController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
-
-Route::view('/signup', 'signup')->name('signup');
 
 Route::controller(AuthController::class)->group(function () {
     Route::get('/login', 'create')->name('auth.create');
     Route::post('/login', 'store')->name('auth.store');
     Route::post('/logout', 'destroy')->name('auth.destroy');
 });
+
+Route::view('/signup', 'signup')->name('signup');
+Route::post('/signup', SignupController::class)->name('signup.post');
 
 Route::resource('news', NewsPostController::class)->parameter('news', 'post');
