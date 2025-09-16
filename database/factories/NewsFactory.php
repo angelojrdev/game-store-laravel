e94ae6a7c7ad5f6093188bcbac1d996a44b8827a
Angelo Oliveira Jr. <43245439+angelojrdev@users.noreply.github.com>
2025-09-16 14:38:21 -0300

    Better NewsFactory

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\News>
@@ -16,9 +18,25 @@ class NewsFactory extends Factory
      */
     public function definition(): array
     {
+        $titleTemplate = Arr::random([
+            'Download ? For Free Today!',
+            '? Coming Soon',
+            '? Game Update',
+            '? Release Date',
+        ]);
+
+        $paragraphs = [];
+        foreach (range(1, rand(3, 7)) as $i) {
+            $paragraphs[] = fake()->realText(750);
+
+            if (rand(1, 5) === 1) {
+                $paragraphs[] = "## Subsection";
+            }
+        }
+
         return [
-            'title' => fake()->jobTitle(),
-            'content' => fake()->realText(),
+            'title' => Str::replace('?', fake()->jobtitle() . ' Simulator', $titleTemplate),
+            'content' => implode("\n\n", $paragraphs)
         ];
     }
 }
